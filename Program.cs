72b687cd4bfd7782e9952e107ff51df1e46e73c0
Kristy Prieto <kprieto14@gmail.com>
2023-02-07 20:22:49 -0500

    Done with explorer mode,onto adventure mode

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace VariablesCS
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      // declaring # of cups of coffee I drink per day
+      var numberOfCupsOfCoffee = 0;
+      // Declaring my full name
+      var fullName = "Kristy Prieto";
+      
+      // Declaring the variables needed to get the day of the week 
+      DateTime now = DateTime.Now;
+      DayOfWeek today = now.DayOfWeek;
+
+      Console.WriteLine($"Hello, my name is {fullName}. Today is {today}, and I have drunk {numberOfCupsOfCoffee} cups of coffee today.");
+      
+      //Asks user what their name is and greets them back.
+      Console.Write("What is your name? ");
+      var name = Console.ReadLine();
+      Console.WriteLine($"It is a pleasure to meet you, {name}! :)");
+
+      //Asks user to enter 2 numbers and store them as strings, then converts them to doubles
+      Console.Write("Please enter your first number: ");
+      string firstNumberAsString = Console.ReadLine();
+      double firstOperand = double.Parse(firstNumberAsString);
+
+      Console.Write("Please enter your second number: ");
+      string secondNumberAsString = Console.ReadLine();
+      double secondOperand = double.Parse(secondNumberAsString);
+
+      // A variety of math operations stored in their corresponding variables, then tells the user some math answers
+      var sum = firstOperand + secondOperand; 
+      var difference = secondOperand - firstOperand;
+      var product = firstOperand * secondOperand;
+      var quotient = firstOperand / secondOperand;
+      var remainder = firstOperand % secondOperand;
+      Console.WriteLine($"Fun facts! If you add {firstNumberAsString} and {secondNumberAsString}, you get {sum}. Isn't that neat?");
+      Console.WriteLine($"What about subtracting {secondNumberAsString} and {firstNumberAsString}? More fun facts, you get {difference}!");
+      Console.WriteLine($"The fun doesn't stop there, what do you get if you multiply {firstNumberAsString} with {secondNumberAsString}? That's right, you get {product}!");
+      Console.WriteLine($"Why stop there? Lets divide {firstNumberAsString} by {secondNumberAsString}. If you guessed {quotient}, you're right!");
+      Console.WriteLine($"And to top it all off, what is the remainder when you divide {firstNumberAsString} by {secondNumberAsString}? That's right, it's {remainder}!");
     }
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # VariablesCS
+
+This is a beginner coding challenge to create a program which reads what the user inputs as a name and two numbers, then gives back some math equations that attempts to emulate fun trivia facts, emphasis on tries. 
